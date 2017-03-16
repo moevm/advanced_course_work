be5ac705648cbd00816f83ee9dd1f83bbf5ec018
AleksandrRybin <Vfibyrf007@gmail.com>
2017-03-16 22:45:14 +0300

    Test

---
+++ Rybin_6304_Generate/Rybin_6304_Generate/Generate.cpp
@@ -0,0 +1,61 @@
+/*!
+\file
+\brief Generating data for checker
+
+This file contains code for generate 2 random tables(csv format)  and integer array of elements [0-5].
+
+\author Rybin Aleksandr 1 course 2 half
+\date 15.03.2017
+\version 1.0
+
+*/
+
+#include "Generate.h"
+
+int main()
+{
+	FILE* Random_Table1 = fopen("input_file_1.csv", "w");
+	FILE* Random_Table2 = fopen("input_file_2.csv", "w");
+
+	fprintf(Random_Table1, "Name,Surname,Patronymic,GitHub,Email,Group\n");
+	fprintf(Random_Table2, "Name,Surname,Exam_mark\n");
+
+	srand(clock());
+	int lenth = 10 + rand() %5;
+
+	Table* Table_obj = (Table*)malloc(sizeof(Table));
+
+	for (int i = 0; i < lenth; i++)
+	{
+
+		generate_field(Table_obj);
+
+		fprintf(Random_Table1, "%s,%s,%s,%s,%s,%d\n",
+			Table_obj->name,
+			Table_obj->surname,
+			Table_obj->patronymic,
+			Table_obj->GitHub_account,
+			Table_obj->Email,
+			Table_obj->group);
+
+		fprintf(Random_Table2, "%s,%s,%d\n",
+			Table_obj->name,
+			Table_obj->surname,
+			Table_obj->exam_result);
+
+		free(Table_obj->Email);
+		free(Table_obj->GitHub_account);
+		free(Table_obj->name);
+		free(Table_obj->surname);
+		free(Table_obj->patronymic);
+		
+	}
+	
+	free(Table_obj);
+
+	fclose(Random_Table1);
+	fclose(Random_Table2);
+
+    return 0;
+}
+
--- Rybin_6304_Generate/Rybin_6304_Generate/Generate.h
+++ Rybin_6304_Generate/Rybin_6304_Generate/Generate.h
@@ -17,9 +17,9 @@ This file contains structs and functions for generate data.
 
 /*!
 \struct
-\brief Struct Table1 for generating data for first table
+\brief Struct Table for generating data for first table
 */
-typedef struct Table1
+typedef struct Table
 {
 	char* name;				//!< Name of student
 	char* surname;			//!< Surame of student
@@ -27,21 +27,11 @@ typedef struct Table1
 	char* GitHub_account;	//!< GitHub account of student
 	char* Email;			//!< Student's email
 	int group;				//!< Student's group
-} Table1;
-
-/*!
-\struct
-\brief Struct Table2 for generating data for second table
-*/
-typedef struct Table2
-{
-	char* name;				//!< Name of student
-	char* surname;			//!< Surame of student
 	int exam_result;		//!< Mark for exam
-} Table2;
+} Table;
 
 /*!
-\brief Generates random integer[1-10000]
+\brief Generates random integer[1-1000]
 \param[in] a Type of random, 0 if group number, 1 if mark for exam, 2 if array of random integer[0-5]
 \return pointer Random integer(pointer) value of group or mark or array
 \ingroup Random
@@ -50,17 +40,22 @@ int* generate_number(int a)
 {
 	int random;
 
-	if (a == 0)
+	switch (a)
+	{
+
+	case 0:
 	{
 		random = 1 + rand() % 1000;
 		return &random;
+		break;
 	}
-	else if (a == 1)
+	case 1:
 	{
 		random = 1 + rand() % 5;
 		return &random;
+		break;
 	}
-	else
+	case 2:
 	{
 
 		int lenth = rand() % 10 + 1;
@@ -74,59 +69,60 @@ int* generate_number(int a)
 
 		return random_array;
 	}
+	}
 }
 
 /*!
 \brief Generates random integer[1-10000]
 \param[in] a Type of random, 0 if just token, 1 if Email token, 2 if GitHub token
-\return random_token Random integer value of group or mark
+\return random_token Random token(char*)
 \ingroup Random
 */
 char* generate_token(int a)
 {
-	int lenth = 5 + rand() %10;
+	int lenth = 5 + rand() % 10;
 	char* random_token = (char*)malloc(sizeof(char) * (lenth + 9));
 
 	for (int i = 0; i < lenth - 1; i++)
 	{
-		random_token[i] = (char)(97 + rand() %25);
+		random_token[i] = (char)(97 + rand() % 25);
 	}
 	random_token[lenth - 1] = '\0';
 
-	if (a == 1)
+	switch (a)
 	{
-		strncat(random_token, "@Email", 6);
-		random_token[lenth + 8] = '\0';
+	case 1:
+		{
+			strncat(random_token, "@Email", 6);
+			random_token[lenth + 8] = '\0';
+			break;
+		}
+	case 2:
+		{
+			strncat(random_token, "@GitHub", 7);
+			random_token[lenth + 8] = '\0';
+			break;
+		}
+	default: break;
 	}
-	if (a == 2)
-	{
-		strncat(random_token, "@GitHub", 7);
-		random_token[lenth + 8] = '\0';
-	}
-	return random_token;
 
+	return random_token;
 }
 
+
 /*!
-\brief Generates table field 1 and 2
-\param[in] Table1_obj struct to write data
-\param[in] Table2_obj struct to write data
+\brief Generates table field 
+\param[in] Table_obj struct to write data
 \return nothing
 \ingroup Random
 */
-void generate_field(Table1* Table1_obj, Table2* Table2_obj)
+void generate_field(Table* Table_obj)
 {
-	char* name = generate_token(0);
-	char * surname = generate_token(0);
-
-	Table1_obj->Email = generate_token(1);
-	Table1_obj->GitHub_account = generate_token(2);
-	Table1_obj->name = name;
-	Table1_obj->surname = surname;
-	Table1_obj->patronymic = generate_token(0);
-	Table1_obj->group = *generate_number(0);
-
-	Table2_obj->name = name;
-	Table2_obj->surname = surname;
-	Table2_obj->exam_result = *generate_number(1);
+	Table_obj->Email = generate_token(1);
+	Table_obj->GitHub_account = generate_token(2);
+	Table_obj->name = generate_token(0);
+	Table_obj->surname = generate_token(0);
+	Table_obj->patronymic = generate_token(0);
+	Table_obj->group = *generate_number(0);
+	Table_obj->exam_result = *generate_number(1);
 }
