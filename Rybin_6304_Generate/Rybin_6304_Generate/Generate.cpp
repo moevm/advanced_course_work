86a2eead4eab916bc025f6b19e7d319b7265b36b
AleksandrRybin <Vfibyrf007@gmail.com>
2017-03-30 11:43:42 +0300

    Test

--- Rybin_6304_Generate/Rybin_6304_Generate/Generate.cpp
+++ Rybin_6304_Generate/Rybin_6304_Generate/Generate.cpp
@@ -2,18 +2,15 @@
 \file
 \brief Generating data for checker
 
-This file contains code for generate 2 random tables(csv format)  and integer array .
+This file contains code for generate 2 random tables(csv format)  and commands_file.txt .
 
 \author Rybin Aleksandr 1 course 2 half
-\date 15.03.2017
-\version 1.0
+\date 30.03.2017
+\version 2.0
 
 */
 
-#include "Data.h"
-
-#define Max_lines 15		//!< Max lines in generating table
-#define Min_lines 5			//!< Min lines in generating table
+#include "Generator.h"
 
 int main()
 {
@@ -25,26 +22,15 @@ int main()
 	FILE* Random_Table2 = fopen("input_file_2.csv", "w");
 	FILE* Commands_file = fopen("commands.txt", "w");
 
-	put_random_array(Commands_file);
+	put_random_commands(Commands_file);
 
 	/* Create table header */
 	fprintf(Random_Table1, "Name,Surname,Patronymic,GitHub,Email,Group\n");
 	fprintf(Random_Table2, "Name,Surname,Exam_mark\n");
 
-	/* Lines in table */
-	int lenth = Min_lines + rand() %Max_lines;
 
-	/* Create a first element of data list */
-	Table* head = generate_field(0);
-	create_random_list(head, lenth);
-
-	/* Put a fields in table */
-	for (int i = 0; i < lenth; i++)
-	{
-		print_field(head, rand() % lenth, Random_Table1, Random_Table2);
-	}
 	
-	free_list(head);
+	
 	fclose(Random_Table1);
 	fclose(Random_Table2);
 
---
+++ Rybin_6304_Generate/Rybin_6304_Generate/Generate_Fun.cpp
@@ -0,0 +1,76 @@
+/*!
+\file
+\brief Generate Functions
+\author Rybin Aleksandr 1 course 2 half
+\date 30.03.2017
+\version 2.0
+
+*/
+
+#include "Generator.h"
+
+void put_random_commands(FILE* commands_list)
+{
+	int lenth = rand() % Max_Lenth_array_commands;
+
+	for (int i = 0; i < lenth; i++)
+		fprintf(commands_list, "%d ", -1 + rand() % 7);
+}
+
+char* generate_token(int a)
+{
+	int lenth = Min_Lenth_token + rand() % Max_Lenth_token;
+	char* random_token = (char*)malloc(sizeof(char) * (lenth + 9));
+
+	for (int i = 0; i < lenth - 1; i++)
+	{
+		random_token[i] = (char)(97 + rand() % 25);
+	}
+	random_token[lenth - 1] = '\0';
+
+	switch (a)
+	{
+	case 1:
+	{
+		strncat(random_token, "@Email", 6);
+		random_token[lenth + 8] = '\0';
+		break;
+	}
+	case 2:
+	{
+		strncat(random_token, "@GitHub", 7);
+		random_token[lenth + 8] = '\0';
+		break;
+	}
+	default: break;
+	}
+
+	return random_token;
+}
+
+void constructor_element(Table element)
+{
+	element.Email = (char*)malloc(sizeof(char) * Max_Lenth_field);
+	element.GitHub_account = (char*)malloc(sizeof(char) * Max_Lenth_field);
+	element.name = (char*)malloc(sizeof(char) * Max_Lenth_field);
+	element.surname = (char*)malloc(sizeof(char) * Max_Lenth_field);
+	element.patronymic = (char*)malloc(sizeof(char) * Max_Lenth_field);
+
+	element.group = 0;
+	element.exam_result = 0;
+	element.check = false;
+
+}
+
+Vector generate_random_vector(int max, int min)
+{
+	Table_obj->Email = generate_token(1);
+	Table_obj->GitHub_account = generate_token(2);
+	Table_obj->name = generate_token(0);
+	Table_obj->surname = generate_token(0);
+	Table_obj->patronymic = generate_token(0);
+	Table_obj->group = 1 + rand() % Max_Group;
+	Table_obj->exam_result = 1 + rand() % Max_Exam_result;
+	Table_obj->number = number;
+
+}
