14614f7a173d2ac32ebef3121895c4fd9ecec35a
Grechkov_lr1 <stas.grechkov@mail.ru>
2017-05-18 22:38:35 +0300

    generator

--- generator/generate.c
+++ generator/generate.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <string.h>
+//#include "MakeName.h"
 #include <time.h>
 
-#define TestNumber 50            // количество случайных тестов
-#define MaxMatrix 20			// максимальное количество матриц в файле
-#define MaxMatrixSize 10
-#define MinMatrixSize 2
-#define MaxElement 100
-#define MinElement -20
+#define TEST_NUMBER 50            // количество случайных тестов
+#define MAX_MATRIX 20			// максимальное количество матриц в файле
+#define MAX_MATRIX_SIZE 10
+#define MIN_MATRIX_SIZE 2
+#define MAX_ELEMENT 100
+#define MIN_ELEMENT -20
 
 int RandCount(int max,int min);		// функция для получения случайного значения
 
@@ -21,18 +22,20 @@ int main()
 	char* filename;
 	int k;
 	FILE* file;
-	for(number_of_files = 0; number_of_files< TestNumber; ++number_of_files)
+	for(number_of_files = 0; number_of_files< TEST_NUMBER; ++number_of_files)
 	{
 		filename = MakeName(number_of_files+1,"test",".txt");
 		file = fopen(filename,"wt");
-		int number_of_matrix = RandCount(MaxMatrix, 1);
+		printf("%s\n",filename);
+		int number_of_matrix = RandCount(MAX_MATRIX, 1);
 		for(k=0; k<number_of_matrix; ++k)
 		{
-			int n = RandCount(MaxMatrixSize,MinMatrixSize);
+			int n = RandCount(MAX_MATRIX_SIZE,MIN_MATRIX_SIZE);
+			int* arr = (int*)malloc(sizeof(int)*n);
 			int m;
 			 if (RandCount(10,0) == 0)
 			 {
-	 			m = RandCount(MaxMatrixSize,MinMatrixSize);   // создаём случайную матрицу
+	 			m = RandCount(MAX_MATRIX_SIZE,MIN_MATRIX_SIZE);   // создаём случайную матрицу
 			 }
 			 else
 			 {
@@ -42,19 +45,28 @@ int main()
 			{
 				for(int j = 0; j < n; ++j)
 				{
-					fprintf(file, "%d ",RandCount(MaxElement,MinElement));   // заполним матрицу случайными числами 
+					if ((number_of_files % 11 != 0) || (i==0))
+					{
+						arr[j] = RandCount(MAX_ELEMENT,MIN_ELEMENT);
+						fprintf(file, "%d ",arr[j]); // заполним матрицу случайными числами
+					}
+				else
+					{
+						fprintf(file, "%d ",2*arr[j]);
+					}
 				}
 				fprintf(file, "\n");
 			}
+			fprintf(file, "\n");
 			fprintf(file, "\n");
 		}
 		fclose(file);
 		free(filename);
 	}
 
-	file = fopen("input","wt");         			//   создадим файл с командами
-	int n = RandCount(MaxMatrix,MaxMatrixSize);		// и заполним его так, чтобы было
-	for(k=0;k<n;++k)								// от 11 до 21 команды
+	file = fopen("input","wt");         			
+	int n = RandCount(2,3);		
+	for(k=0;k<n;++k)								
 	{
 		fprintf(file, "%d ",RandCount(3,1));
 	}
@@ -81,4 +93,3 @@ char* MakeName(int index,char* name, char* add)
 	strcat(result,add);
 	return result;
 }
-
