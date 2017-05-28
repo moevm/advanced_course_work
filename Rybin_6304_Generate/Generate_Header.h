d92fb817d37f1f86c0227efd73760d856946de32
AleksandrRybin <Vfibyrf007@gmail.com>
2017-05-28 19:13:33 +0300

    DONE

--- Rybin_6304_Generate/Generate_Header.h
+++ Rybin_6304_Generate/Generate_Header.h
@@ -2,8 +2,23 @@
 \file
 \brief Generate Header
 \author Rybin Aleksandr 1 course 2 half
-\date 30.03.2017
-\version 2.0
+\date 21.05.2017
+\version 3.0
+*/
+
+/*!
+\mainpage
+- Creating the checker for work with csv tables. This project contain:
+- Generator that creates two random tables and file with sets of comands
+- Refsol that work with tables on right way
+- Run.sh bash script that runs Refsol and user solution and check results
+\author Rybin Aleksandr 1 course 2 half group 6304 FKTI
+\date 28.05.2017
+\version 4.0
+*/
+
+/*!
+\defgroup Generate Functions to generate
 */
 
 #include <stdio.h>
@@ -11,18 +26,18 @@
 #include <time.h>
 #include <string.h>
 
-#define MAX_LENGTH_ARRAY_COMMANDS 10		//!< Max length of generating array of commands
-#define MAX_LENGTH_TOKEN 15				//!< Max length of generating random token
-#define MIN_LENGTH_TOKEN 5				//!< Min length of generating random token
-#define MAX_GROUP 1000					//!< Max generating group number
-#define MAX_EXAM_RESULT 5				//!< Max generating exam result
-#define MAX_LINES 20					//!< Max lines in generating tables
-#define MIN_LINES 15					//!< Min lines in generating tables
-#define HEADER1 "Name,Surname,Patronymic,GitHub,Email,Group\n" //!< Header for first table
-#define HEADER2 "Name,Surname,Exam_mark\n"					   //!< Header for second table
-#define INPUT1 "input_file_1.csv"		//!< First table
-#define INPUT2 "input_file_2.csv"		//!< Second table
-#define COMMANDS "commands.txt"			//!< File with commands
+#define MAX_LENGTH_ARRAY_COMMANDS 10	                         	//!< Max length of generating array of commands
+#define MAX_LENGTH_TOKEN 15				                           	//!< Max length of generating random token
+#define MIN_LENGTH_TOKEN 5				                  			//!< Min length of generating random token
+#define MAX_GROUP 1000					              				//!< Max generating group number
+#define MAX_EXAM_RESULT 5	                          				//!< Max generating exam result
+#define MAX_LINES 15			                            		//!< Max lines in generating tables
+#define MIN_LINES 5					                       			//!< Min lines in generating tables
+#define HEADER1 "Name,Surname,Patronymic,GitHub,Email,Group\n"      //!< Header for first table
+#define HEADER2 "Name,Surname,Exam_mark\n"					        //!< Header for second table
+#define INPUT1 "input_file_1.csv"		                            //!< First table
+#define INPUT2 "input_file_2.csv"		                            //!< Second table
+#define COMMANDS "commands.txt"			                            //!< File with commands
 
 /*!
 \struct
@@ -30,15 +45,15 @@
 */
 typedef struct Table
 {
-	char* name;				//!< Name of student
-	char* surname;			//!< Surame of student
-	char* patronymic;		//!< Patronymic of student
-	char* GitHub_account;	//!< GitHub account of student
-	char* Email;			//!< Student's email
-	int group;				//!< Student's group
-	int exam_result;		//!< Mark for exam
-	int flag;				//!< If repeats - 1, if last in group -1, default 0
-} Table;
+	char name[MAX_LENGTH_TOKEN];				                    //!< Name of student
+	char surname[MAX_LENGTH_TOKEN];			                        //!< Surame of student
+	char patronymic[MAX_LENGTH_TOKEN];		                        //!< Patronymic of student
+	char GitHub_account[MAX_LENGTH_TOKEN];	                        //!< GitHub account of student
+	char Email[MAX_LENGTH_TOKEN];	                                //!< Student's email
+	int group;				                                        //!< Student's group
+	int exam_result;		                                        //!< Mark for exam
+	int flag;				                                        //!< If repeats 1, if last in group (-1), default 0
+}Table;
 
 /*!
 \struct
@@ -46,8 +61,8 @@ typedef struct Table
 */
 typedef struct Vector
 {
-	int lenth;				//!< Lenth of vector 
-	Table* vector_ptr;		//!< Pointer to vector
+	int len;				                                        //!< Length of vector
+	Table vector_ptr[MAX_LINES];		                            //!< Pointer to struct Table arrayr
 }Vector;
 
 /*!
@@ -60,11 +75,11 @@ void PutRandomCommands(FILE* commands_list);
 
 /*!
 \brief Generates random token
-\param[in] a Type of random, 0 if just token, 1 if @Email token, 2 if @GitHub token
-\return random_token Pointer to random token
+\param[in] random_token Pointer to string to write random token
+\return void
 \ingroup Generate
 */
-char* GenerateToken(int a);
+void GenerateToken(char* random_token);
 
 /*!
 \brief Generates random vector of fields
